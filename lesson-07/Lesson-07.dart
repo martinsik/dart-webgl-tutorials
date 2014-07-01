f51e12cb67863e9188ca246dda6af16a8cfd0b0f
Horsetopus <horsetopus@gmail.com>
2014-07-02 00:05:21 +0200

    Update Lesson-07.dart

--- lesson-07/Lesson-07.dart
+++ lesson-07/Lesson-07.dart
@@ -108,21 +108,6 @@ class Lesson07 {
   void _initShaders() {
     // vertex shader source code. uPosition is our variable that we'll
     // use to create animation
-    /*String vsSource = """
-    attribute vec3 aVertexPosition;
-    attribute vec2 aTextureCoord;
-  
-    uniform mat4 uMVMatrix;
-    uniform mat4 uPMatrix;
-  
-    varying vec2 vTextureCoord;
-  
-    void main(void) {
-      gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
-      vTextureCoord = aTextureCoord;
-    }
-    """;*/
-
     String vsSource = """
     attribute vec3 aVertexPosition;
     attribute vec3 aVertexNormal;
